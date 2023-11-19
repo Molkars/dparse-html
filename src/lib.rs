206370d0e02ff92cc124d231ee4deaa142e95ca2
Molkars <molkars@outlook.com>
2023-11-18 19:50:19 -0700

    initial

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,10 @@ mod attribute;
 mod opening_tag;
 mod token;
 mod tag;
+
+pub use tag::*;
+pub use token::*;
+pub use opening_tag::*;
+pub use attribute::*;
+pub use html_ident::*;
+pub use html_lit_str::*;
